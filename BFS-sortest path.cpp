635d4e65a41b278607b109a301df23704bcb231d
Blue Coder <firedeath.lu@gmail.com>
2016-08-08 03:17:27 +0600

    Update BFS-sortest path.cpp

--- BFS-sortest path.cpp
+++ BFS-sortest path.cpp
@@ -71,6 +71,15 @@ int main(){
 6 8 2
 7 8 2
 
+0 1 1 3 0 0 0 0
+1 0 3 0 4 2 0 0
+1 3 0 0 2 3 0 0
+3 0 0 0 1 0 4 0
+0 4 2 1 0 4 3 0
+0 2 3 0 4 0 3 2
+0 0 0 4 3 3 0 2
+0 0 0 0 0 2 2 0
+
 overall all node's sortest path -->
 
 0 1 1 3 3 3 6 5       sortest path distance from 1 to all node
