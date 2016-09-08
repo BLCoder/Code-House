50f4cb7a84d371e7390caec2db11cbc1a32194cd
SR_JVD <firedeath.lu@gmail.com>
2016-09-08 20:56:58 +0600

    Update Segment Tree(MIN).cpp

--- Segment Tree(MIN).cpp
+++ Segment Tree(MIN).cpp
@@ -39,7 +39,6 @@ int main(){
     int n;
     scanf("%d",&n);
     for(int i=0;i<n;i++) scanf("%d",&arr[i]);
-    for(int i=0;i<n*3;i++) seg[i]=1e9;
     build(arr,seg,0,n-1,0);
     cout<<quary(seg,2,7,0,n-1,0);
 }
