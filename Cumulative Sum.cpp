b1a90bd02e33ee1a4a7e94b6646aa20a7967fb2b
SR_JVD <firedeath.lu@gmail.com>
2016-10-09 16:52:44 +0600

    minor change

--- Cumulative Sum.cpp
+++ Cumulative Sum.cpp
@@ -3,7 +3,7 @@
 | / | / | / | / | / |
 4   5   7   13  21  23
 
-
+sum of lo to hi =   sum[hi]-sum[lo-1]
 */
 #include<bits/stdc++.h>
 using namespace std;
@@ -25,7 +25,7 @@ int main(){
     cumilative();
     while(1){
         int lo,hi;
-        scanf("%d%d",&lo,&hi);
+        scanf("%d%d",&lo,&hi);   //1 base
         printf("%d ",sum[hi-1]-sum[lo-2]);
         int s=0;
         for(int i=lo-1;i<hi;i++) s+=ar[i];
